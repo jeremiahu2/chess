df89b2b693535b9bdf9440539c3d0d49e16eb5cf
Jeremiah Barton <boateng@byu.edu>
2025-10-23 17:43:02 -0600

    Added curly braces to GameHandler

--- server/src/main/java/handlers/GameHandler.java
+++ server/src/main/java/handlers/GameHandler.java
@@ -25,14 +25,16 @@ public class GameHandler {
             List<GameData> games = gameService.listGames(token);
             ctx.status(200).json(Map.of("games", games));
         } catch (DataAccessException e) {
-            if ("unauthorized".equals(e.getMessage())) ctx.status(401).json(Map.of("message", "Error: unauthorized"));
-            else ctx.status(500).json(Map.of("message", "Error: " + e.getMessage()));
+            if ("unauthorized".equals(e.getMessage())) {
+                ctx.status(401).json(Map.of("message", "Error: unauthorized"));
+            } else {
+                ctx.status(500).json(Map.of("message", "Error: " + e.getMessage()));
+            }
         } catch (Exception e) {
             ctx.status(500).json(Map.of("message", "Error: " + e.getMessage()));
         }
     }
 
-    // POST /game
     public void createGame(Context ctx) {
         try {
             String token = ctx.header("authorization");
@@ -41,15 +43,18 @@ public class GameHandler {
             ctx.status(200).json(res);
         } catch (DataAccessException e) {
             String msg = e.getMessage();
-            if ("unauthorized".equals(msg)) ctx.status(401).json(Map.of("message", "Error: unauthorized"));
-            else if ("bad request".equals(msg)) ctx.status(400).json(Map.of("message", "Error: bad request"));
-            else ctx.status(500).json(Map.of("message", "Error: " + msg));
+            if ("unauthorized".equals(msg)) {
+                ctx.status(401).json(Map.of("message", "Error: unauthorized"));
+            } else if ("bad request".equals(msg)) {
+                ctx.status(400).json(Map.of("message", "Error: bad request"));
+            } else {
+                ctx.status(500).json(Map.of("message", "Error: " + msg));
+            }
         } catch (Exception e) {
             ctx.status(500).json(Map.of("message", "Error: " + e.getMessage()));
         }
     }
 
-    // PUT /game
     public void joinGame(Context ctx) {
         try {
             String token = ctx.header("authorization");
@@ -58,10 +63,15 @@ public class GameHandler {
             ctx.status(200).json(Map.of());
         } catch (DataAccessException e) {
             String msg = e.getMessage();
-            if ("unauthorized".equals(msg)) ctx.status(401).json(Map.of("message", "Error: unauthorized"));
-            else if ("already taken".equals(msg)) ctx.status(403).json(Map.of("message", "Error: already taken"));
-            else if ("bad request".equals(msg)) ctx.status(400).json(Map.of("message", "Error: bad request"));
-            else ctx.status(500).json(Map.of("message", "Error: " + msg));
+            if ("unauthorized".equals(msg)) {
+                ctx.status(401).json(Map.of("message", "Error: unauthorized"));
+            } else if ("already taken".equals(msg)) {
+                ctx.status(403).json(Map.of("message", "Error: already taken"));
+            } else if ("bad request".equals(msg)) {
+                ctx.status(400).json(Map.of("message", "Error: bad request"));
+            } else {
+                ctx.status(500).json(Map.of("message", "Error: " + msg));
+            }
         } catch (Exception e) {
             ctx.status(500).json(Map.of("message", "Error: " + e.getMessage()));
         }
