e18bf4d3a2356eb8b431bb3701b7ca50a0c588c8
Jeremiah Barton <boateng@byu.edu>
2025-10-23 18:17:25 -0600

    updated Server.java to fix the "web" issue

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -9,53 +9,53 @@ import handlers.UserHandler;
 import handlers.SessionHandler;
 import handlers.GameHandler;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Map;
 
 public class Server {
-    private final Javalin javalin;
+    private Javalin javalin;
     private final DataAccess dao;
     private final UserService userService;
     private final GameService gameService;
 
     public Server() {
-        this.javalin = Javalin.create(config -> config.staticFiles.add("web"));
         this.dao = new InMemoryDataAccess();
         this.userService = new UserService(dao);
         this.gameService = new GameService(dao);
-
-        registerEndpoints();
     }
 
     private void registerEndpoints() {
         var userHandler = new UserHandler(userService);
         var sessionHandler = new SessionHandler(userService);
         var gameHandler = new GameHandler(gameService);
-
-        // Clear DB
         javalin.delete("/db", ctx -> {
             dao.clear();
             ctx.status(200).json(Map.of());
         });
-
-        // Register
         javalin.post("/user", userHandler::register);
-
-        // Login & Logout
         javalin.post("/session", sessionHandler::login);
         javalin.delete("/session", sessionHandler::logout);
-
-        // Games
         javalin.get("/game", gameHandler::listGames);
         javalin.post("/game", gameHandler::createGame);
         javalin.put("/game", gameHandler::joinGame);
     }
 
     public int run(int desiredPort) {
-        javalin.start(desiredPort);
+        javalin = Javalin.create(config -> {
+            Path webDir = Path.of("web");
+            if (Files.exists(webDir)) {
+                config.staticFiles.add(webDir.toString());
+            }
+        }).start(desiredPort);
+
+        registerEndpoints();
         return javalin.port();
     }
 
     public void stop() {
-        javalin.stop();
+        if (javalin != null) {
+            javalin.stop();
+        }
     }
 }
