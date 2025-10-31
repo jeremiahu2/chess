ee32726e4c6866d49478e248c479285f8cba83e2
Jeremiah Barton <boateng@byu.edu>
2025-10-31 17:17:38 -0600

    Added ClearHandler

---
+++ server/src/main/java/handlers/ClearHandler.java
@@ -0,0 +1,26 @@
+package handlers;
+
+import com.google.gson.Gson;
+import io.javalin.http.Context;
+import service.ClearService;
+import dataaccess.DataAccessException;
+
+import java.util.Map;
+
+public class ClearHandler {
+    private final ClearService clearService;
+    private final Gson gson = new Gson();
+
+    public ClearHandler(ClearService clearService) {
+        this.clearService = clearService;
+    }
+
+    public void clear(Context ctx) {
+        try {
+            clearService.clear();
+            ctx.status(200).result(gson.toJson(Map.of())); // return empty JSON object
+        } catch (DataAccessException e) {
+            ctx.status(500).result(gson.toJson(Map.of("message", "Error: " + e.getMessage())));
+        }
+    }
+}
