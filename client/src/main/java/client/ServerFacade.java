947cb37704698f540c109716059d041b9ee277ab
Jeremiah Barton <boateng@byu.edu>
2025-11-13 22:25:30 -0700

    Updated ServerFacade.java

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,7 +1,14 @@
 package client;
 
 import com.google.gson.Gson;
+
+import java.net.URI;
 import java.net.http.*;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.rmi.ServerException;
+import com.google.gson.reflect.TypeToken;
+import java.util.Map;
 
 public class ServerFacade {
     private final String baseUrl;
@@ -12,5 +19,28 @@ public class ServerFacade {
     public ServerFacade(int port) {
         this.baseUrl = "http://localhost:" + port;
         this.client = HttpClient.newBuilder().build();
+    }
+
+    private HttpRequest.Builder jsonRequestBuilder(String path) {
+        HttpRequest.Builder b = HttpRequest.newBuilder()
+                .uri(URI.create(baseUrl + path))
+                .header("Content-Type", "application/json");
+        if (authToken != null) {
+            b.header("Authorization", "Bearer " + authToken);
+        }
+        return b;
+    }
+
+    private <T> T sendPost(String path, Object body, Class<T> clazz) throws IOException, InterruptedException, ServerException {
+        String json = GSON.toJson(body);
+        HttpRequest req = jsonRequestBuilder(path)
+                .POST(HttpRequest.BodyPublisher.ofString(json, StandardCharsets.UTF_8))
+                .build();
+        return send(req, clazz);
+    }
+
+    private <T> T sendGet(String path, Class<T> clazz) throws IOException, InterruptedException, ServerException {
+        HttpRequest req = jsonRequestBuilder(path).GET().build();
+        return send(req, clazz);
     }
 }
